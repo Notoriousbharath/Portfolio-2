af916d6e220d7d588daa4ba8ed9b737bfa3ccdba
bharath <bwaj9078@gmail.com>
2024-10-23 17:06:46 +0530

    Updated Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
   return (
     <div className="border border-gray-600 bg-black text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-6 place-items-center md:flex md:justify-between md:items-center">
       <h2 className="text-gray-700 text-xl md:text-4xl font-bold m-4">
-        My <br /> Teach <br /> Stack
+        My <br /> Tech <br /> Stack
       </h2>
       <div className="flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]">
         <img src={html} alt="" width={100} height={100} />
